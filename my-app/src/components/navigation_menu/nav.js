b3d12a24cdf644eff1c87ea76ac461dbdb6d299c
Sun-007 <78736365+Umesh-daiict@users.noreply.github.com>
2023-12-23 22:19:38 +0530

    nav bar pratice compelated

--- my-app/src/components/navigation_menu/nav.js
+++ my-app/src/components/navigation_menu/nav.js
@@ -3,43 +3,33 @@ import styles from "./nav.module.css";
 
 export default function NavMenu() {
   return (
-    <nav className={styles.nav}>
-      <ul className={styles.ul}>
-        <li className={styles.logo}>
-          <Image
-            src="/vercel.svg"
-            alt="Logo"
-            className={styles.vercelLogo}
-            width={100}
-            height={24}
-            priority
-          />
-        </li>
+		<nav className={styles.nav}>
+			<ul className={styles.ul}>
+				<li className={styles.li}>
+					<Image
+						src='/vercel.svg'
+						alt='Logo'
+						className={styles.logo}
+						width={100}
+						height={24}
+						priority
+					/>
+				</li>
 
-        {[
-          { className: "", label: "Home", id: 1 },
-          { className: "", label: "About", id: 2 },
-          { className: "", label: "Services", id: 3 },
-          { className: "", label: "Const", id: 4 },
-          { className: "", label: "Home", id: 5 },
-          { className: "", label: "Home", id: 6 },
-          { className: "", label: "Home", id: 7 },
-        ].map((item) => (
-          <li key={item.id} className={""}>
-            {item.label}
-          </li>
-        ))}
-
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  );
+				{[
+					{ className: '', label: 'Home', id: 1 },
+					{ className: '', label: 'About', id: 2 },
+					{ className: '', label: 'Services', id: 3 },
+					{ className: '', label: 'Const', id: 4 },
+					{ className: '', label: 'Home', id: 5 },
+					{ className: '', label: 'Home', id: 6 },
+					{ className: '', label: 'Home', id: 7 },
+				].map((item) => (
+					<li key={item.id} className={styles.li}>
+						{item.label}
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
 }
